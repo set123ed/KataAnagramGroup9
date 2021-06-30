6723d1a7d4527c22dfc9dac31d1b592ee83cda0d
ENMANUEL URENA VARGAS <1095352@est.intec.edu.do>
2021-06-29 20:18:10 -0400

    anagramCheck

--- Anagrams.py
+++ Anagrams.py
@@ -1,2 +1,16 @@
-def Sanitize(word):
+from typing import Dict
+
+def sanitize(word):
   return str(sorted(word))
+
+dic: Dict = {}
+
+def check(word):
+    key = sanitize(word)
+    match = key in dic
+    if match:
+        dic[key].append(word)
+    else: 
+        dic.setdefault(key, [])
+        dic[key].append(word)
+    return match
--- Anagrams_Test.py
+++ Anagrams_Test.py
@@ -4,4 +4,12 @@ import Anagrams as Anagram
 def test_Sanitize():
   word = "hola"
   result = "['a', 'h', 'l', 'o']"
-  assert Anagram.Sanitize(word) == result
+  assert Anagram.sanitize(word) == result
+
+def test_isAnagram():
+    word1 = "romo"
+    word2 = "moro"
+    Anagram.check(word1)
+    assert Anagram.check(word2) == True
+
+
